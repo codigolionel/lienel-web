4dad53442761e54ab72662dfac0a66ad47d4417f
Lionel <augustolionel@gmail.com>
2026-04-02 01:35:25 -0300

    Finish2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -234,7 +234,7 @@ const Hero = () => {
                 >
                     <span className="relative flex h-2 w-2">
                         <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-[#00D2D3] opacity-75"></span>
-                        <span className="relative inline-flex rounded-full h-1 w-2 bg-[#00D2D3]"></span>
+                        <span className="relative inline-flex rounded-full h-2 w-2 bg-[#00D2D3]"></span>
                     </span>
                     Diseño Web
                 </div>
